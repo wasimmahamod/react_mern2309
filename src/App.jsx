c3e41ec506d96ad46c19f4c263d28f17db88c511
wasimmahamod <wasimmahamodraza@gmail.com>
2024-09-02 20:44:30 +0600

    react project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quam, expedita quibusdam nulla iste excepturi perferendis necessitatibus enim obcaecati odio?</h1>
+    </div>
+  )
+}
+
+export default App
